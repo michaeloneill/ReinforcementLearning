85ba15d29eaf75358dbffd01c0a2a87659f85101
michaeloneill <michael.oneill86@gmail.com>
2018-06-15 09:23:27 +0100

    running lamda plot again

--- lib/plotting.py
+++ lib/plotting.py
@@ -4,24 +4,29 @@ import pandas as pd
 from collections import namedtuple
 from matplotlib import pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
 
 EpisodeStats = namedtuple("Stats",["episode_lengths", "episode_rewards"])
 
-def plot_cost_to_go_mountain_car(env, estimator, num_tiles=20):
+def plot_cost_to_go_mountain_car(env, estimator, num_tiles=50):
     x = np.linspace(env.observation_space.low[0], env.observation_space.high[0], num=num_tiles)
     y = np.linspace(env.observation_space.low[1], env.observation_space.high[1], num=num_tiles)
     X, Y = np.meshgrid(x, y)
     Z = np.apply_along_axis(lambda _: -np.max(estimator.predict(_)), 2, np.dstack([X, Y]))
 
     fig = plt.figure(figsize=(10, 5))
-    ax = fig.add_subplot(111, projection='3d')
-    surf = ax.plot_surface(X, Y, Z, rstride=1, cstride=1,
-                           cmap=matplotlib.cm.coolwarm, vmin=-1.0, vmax=1.0)
+    #ax = fig.add_subplot(111, projection='3d')
+    #surf = ax.plot_surface(X, Y, Z, rstride=1, cstride=1,
+    #                       cmap=matplotlib.cm.coolwarm, vmin=-1.0, vmax=1.0)
+    ax = fig.add_subplot(111)
+    p = ax.pcolor(X, Y, Z, cmap=cm.RdBu, vmin=0, vmax=200)
+    cb = fig.colorbar(p)
+
     ax.set_xlabel('Position')
     ax.set_ylabel('Velocity')
-    ax.set_zlabel('Value')
+    #ax.set_zlabel('Value')
     ax.set_title("Mountain \"Cost To Go\" Function")
-    fig.colorbar(surf)
+    #fig.colorbar(surf)
     plt.show()
 
 
