959610a34d969012aa493ef62574012407f20bb5
michaeloneill <michael.oneill86@gmail.com>
2018-06-18 09:23:20 +0100

    refactor and test

--- lib/.ipynb_checkpoints/plotting-checkpoint.py
+++ lib/.ipynb_checkpoints/plotting-checkpoint.py
@@ -10,7 +10,7 @@ def plot_cost_to_go(env, estimator, num_tiles=50):
     y = np.linspace(env.observation_space.low[1], env.observation_space.high[1], num=num_tiles)
     X, Y = np.meshgrid(x, y)
     Z = np.apply_along_axis(
-        lambda x, y: -np.max(estimator.predict([x, y])), 2, np.stack([X, Y], axis=2))
+        lambda obs: -np.max(estimator.predict(obs)), 2, np.stack([X, Y], axis=2))
 
     fig, ax = plt.subplots(figsize=(10, 5))
     p = ax.pcolor(X, Y, Z, cmap=cm.RdBu, vmin=0, vmax=200)
@@ -26,7 +26,6 @@ def plot_learning_curves(stats, smoothing_window=10):
     
     plt.figure(figsize=(10,5))
     for algo, steps_per_episode in stats.items():
-        plt.figure(figsize=(10,5))
         steps_per_episode = pd.Series(steps_per_episode).rolling(
             smoothing_window).mean()  # smooth
         plt.plot(steps_per_episode, label=algo)
@@ -37,12 +36,12 @@ def plot_learning_curves(stats, smoothing_window=10):
     plt.show()
 
 
-def run_grid_search(algorithm, estimator, alphas, bootstrappings, episodes=100, runs=5,
+def run_grid_search(algorithm, estimator, env, alphas, bootstrappings, episodes=100, runs=5,
                     truncate_steps=400, bootstrapping_descriptor='bootstrapping'):
     
     estimator.reset()
     steps = np.zeros((len(bootstrappings), len(alphas)))
-    for run in range(0, runs):
+    for run in range(runs):
         for b_idx, bootstrapping in enumerate(bootstrappings):
             for a_idx, alpha in enumerate(alphas):
                 for episode in range(episodes):
@@ -51,7 +50,7 @@ def run_grid_search(algorithm, estimator, alphas, bootstrappings, episodes=100, 
                             run, bootstrapping_descriptor, 
                             bootstrapping, alpha, episode), end="")
                     n_steps, _ = algorithm(bootstrapping, env=env, estimator=estimator)
-                    steps[lmbdaIndex, alphaIndex] += n_steps
+                    steps[b_idx, a_idx] += n_steps
     
     # average over independent runs and episodes
     steps /= runs * episodes
@@ -60,7 +59,7 @@ def run_grid_search(algorithm, estimator, alphas, bootstrappings, episodes=100, 
     steps[steps > truncate_steps] = truncate_steps
 
     plt.figure()
-    for b_idx in range(len(bootstrappings):
+    for b_idx in range(len(bootstrappings)):
         plt.plot(alphas, steps[b_idx, :], 
             label='{}: {}'.format(boostrapping_destriptor, bootstrappings[b_idx]))
     plt.xlabel('alpha * number of tilings(8)')
