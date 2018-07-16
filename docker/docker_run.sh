3934a65662f73c41a5e751763b2e1deb77c28261
michaeloneill <michael.oneill86@gmail.com>
2018-07-16 22:05:19 +0100

    change folder name and remove equation numbering

--- docker/docker_run.sh
+++ docker/docker_run.sh
@@ -1,14 +1,8 @@
 docker build -t gym .
 
-# nvidia-docker run -it \
-#       -v /nfs_mount:/nfs_mount \
-#       -v /home/michael/Documents/Kheiron:/src \
-#       --name=michael_container \
-#       michael
-
 docker run -it \
        -p 8888:8888 \
-       -v /Users/michaeloneill/Documents/RL:/src/RL \
+       -v /Users/michaeloneill/Documents/ReinforcementLearning:/src/ReinforcementLearning \
        --env="DISPLAY" \
        --env="QT_X11_NO_MITSHM=1" \
        --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
